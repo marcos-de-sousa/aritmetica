faa657b5d3f7f638248640fc7bafd1d60da15104
Marcos Sousa <marcossousa@MacBook-Air-de-Marcos-5.local>
2020-09-29 21:17:09 -0300

    Initial commit

---
+++ calcular.py
@@ -0,0 +1,6 @@
+from aritmetica import operacoes
+
+print("soma:",operacoes.soma(4,2))
+print("subtração:",operacoes.subtracao(4,2))
+print("multiplicação:",operacoes.multiplicacao(4,2))
+print("divisão: ",operacoes.divisao(4,2))
