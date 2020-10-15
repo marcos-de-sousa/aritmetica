b13771f62cd3bef6c6ed1b087d9ed2d1f6536157
Marcos de Sousa <71830780+marcos-de-sousa@users.noreply.github.com>
2020-10-15 15:02:05 -0300

    Atualização do setup.py

--- setup.py
+++ setup.py
@@ -10,3 +10,21 @@ setup(
     author_email='msousa@museu-goeldi.br',
     description='Pacote python para operações aritméticas'
 )
+
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    readme = fh.read()
+
+setup(name='aritmetica',
+    version='0.0.1',
+    url='https://github.com/marcos-de-sousa/aritmetica',
+    license='MIT License',
+    author='Marcos Paulo Alves de Sousa',
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author_email='desousa.mpa@gmail.com',
+    keywords='Pacote',
+    description=u'Exemplo de pacote PyPI',
+    packages=['aritmetica'],
+    install_requires=['python3.6'],)
